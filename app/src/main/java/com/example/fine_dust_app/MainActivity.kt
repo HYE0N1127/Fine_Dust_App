c2d8955b6d7ed48d09218aa33de453de32283b3f
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-04-09 23:02:09 +0900

    ADD : Glide 추가

--- app/src/main/java/com/example/fine_dust_app/MainActivity.kt
+++ app/src/main/java/com/example/fine_dust_app/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.fine_dust_app
 
+import android.app.Activity
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.fine_dust_app.data.model.GithubInfo
 import com.example.fine_dust_app.data.retrofit_builder.RetrofitBuilder
 import retrofit2.Call
@@ -11,15 +15,16 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tvBio : TextView
-    lateinit var ivProfile : TextView
+    lateinit var tvBio: TextView
+    lateinit var ivProfile: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tvBio = findViewById(R.id.tvBio)
-        ivProfile = findViewById(R.id.ivProfile)
+        val imageView: ImageView = findViewById(R.id.ivProfile)
 
         val call = RetrofitBuilder().githubApi.getGithubInfo()
 
@@ -30,18 +35,22 @@ class MainActivity : AppCompatActivity() {
                 Log.d(
                     "response",
                     "${userInfo?.bio} ${userInfo?.login} ${userInfo?.followers} ${userInfo?.following} ${userInfo?.html_url} ${userInfo?.name}" +
-                            "${userInfo?.company} ${userInfo?.location} ${userInfo?.blog}"
+                            "${userInfo?.company} ${userInfo?.location} ${userInfo?.blog} ${userInfo?.avatar_url}"
                 )
                 tvBio.text = userInfo?.bio.toString()
-                ivProfile. = userInfo?.avatar_url
-
             }
 
             override fun onFailure(call: Call<GithubInfo>, t: Throwable) {
                 Log.d("error", t.message.toString())
             }
 
+            fun getImageURL(imageURL: String) {
 
+                imageURL =
+
+                Glide.with(Activity()).
+                load(imageURL)
+            }
         })
     }
 }
